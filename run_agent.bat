d4c708765a3f7cc27ebd9d7cdf5096ec80e2519c
KobiLDN <kobildn@gmail.com>
2026-05-18 23:37:03 +0100

    fix: use agent.fetch_xg in bat preflight instead of raw client call

--- run_agent.bat
+++ run_agent.bat
@@ -2,7 +2,7 @@
 cd /d "G:\My Drive\coding\ai\football-analyser\football-analyserDEV"
 
 echo Testing Understat xG fetch...
-python -c "from understatapi import UnderstatClient; uc=UnderstatClient(); m=uc.team('Arsenal').get_match_data('2025'); uc.close(); played=[x for x in m if x.get('isResult')][-1]; print('  xG OK - last match xG:', played['xG'], 'xGA:', played['xGA'])"
+python -c "from agent import fetch_xg; r=fetch_xg('Arsenal','pl'); print('  xG OK:', r) if r else print('  WARNING: xG returned None')"
 echo.
 
 echo Running agent...
